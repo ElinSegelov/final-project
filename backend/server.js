f6c2e9cb3729199a62302e663de403a32799b9f9
Elin <elin.segelov@gmail.com>
2022-12-13 12:01:29 +0100

    Add PATCH and DELETE endspoints to /user. Update GET /event to send different information depending on if a user is authenticated

--- backend/server.js
+++ backend/server.js
@@ -108,7 +108,6 @@ app.get("/", (req, res) => {
 login
 register
 events (PATCH, DELETE)
-user-profile (GET, PATCH, DELETE )
 
 */
 
@@ -134,11 +133,11 @@ const authenticateUser = async (req, res, next) => {
   }
 }
 
+const salt = bcrypt.genSaltSync()
 // -------------------------------- USER --------------------------------
 app.post("/register", async (req, res) => {
   const { username, password, email } = req.body;
   try {
-    const salt = bcrypt.genSaltSync()
     if (password.lenght < 8) {
       res.status(400).json({
         success: false,
@@ -198,14 +197,12 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/user-profile", authenticateUser);
-app.get("/user-profile", async (req, res) => {
-  // FAAAAAAAAN!!!!! DUKTIG!!!
-  
+app.get("/user", authenticateUser);
+app.get("/user", async (req, res) => {  
   let allUserInfo = [];
   const selectedUser = await User.findOne({accessToken: req.header("Authorization")});
   allUserInfo.push(selectedUser);
-// see if this can be done smoother
+// see if this can be done smoother - create new object
   const userInfo = allUserInfo.map((user) => {
     return ({
       username: user.username,
@@ -224,6 +221,73 @@ app.get("/user-profile", async (req, res) => {
       success: false,
       response: err
     })
+  }
+});
+
+
+// CHECK OUT IF WE CAN SIMPLIFY THIS BLOCK
+// THIS ALLOWS THE USER TO CHANGE THE EMAIL AND THE PASSWORD
+app.patch("/user", authenticateUser);
+app.patch("/user", async (req, res) => {
+  const { newEmail, newPassword } = req.body;
+  try {
+    const selectedUser = await User.findOne({accessToken: req.header("Authorization")});
+    if (newEmail && newPassword) {
+      await User.findByIdAndUpdate(selectedUser._id, {$set: {email: newEmail, password: bcrypt.hashSync(newPassword, salt)}})
+      res.status(200).json({
+        success: true,
+        response: {
+          message: "Your credentials have been updated"
+        }})      
+  } else if (newPassword) {
+    await User.findByIdAndUpdate(selectedUser._id, {$set: {password: bcrypt.hashSync(newPassword, salt)}})
+    res.status(200).json({
+      success: true,
+      response: {
+        message: "Your password have been updated"
+      }})      
+    } else if (newEmail) {
+      await User.findByIdAndUpdate(selectedUser._id, {$set: { email: newEmail}})
+      res.status(200).json({
+        success: true,
+        response: {
+          message: "Your email have been updated"
+        }})    
+  }
+} catch (err) {
+  res.status(400).json({
+    success: false,
+    response: err
+  })
+}
+})
+
+app.delete("/user", authenticateUser);
+app.delete("/user", async (req, res) => {
+  try {
+    const deletedUser = await User.findOneAndDelete({accessToken: req.header("Authorization")})
+    if (deletedUser) {
+      res.status(200).json({
+        success: true,
+        response: {
+          deletedUser: deletedUser.username,
+          message: "Your account has been deleted"
+        }
+    })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: {
+          message: "Ooops! Something went wrong. Please try again later."
+        }
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      response: err
+    })
+
   }
 });
 
@@ -259,23 +323,9 @@ app.post("/event", async (req, res) => {
   }
 });
 
-app.get("/event", authenticateUser)
+//If user is not authnticated they get limited info about the events, if user is authenticated they get all event info.
 app.get("/event", async (req, res) => {
-  const allEvents = await Event.find().sort({ createdAt: "desc" });
-  try {
-    res.status(200).json({
-      success: true,
-      response: allEvents
-    })
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      response: err
-    })
-  }
-})
-
-app.get("/event/public", async (req, res) => {
+  const accessToken = req.header("Authorization");
   const allEvents = await Event.find().sort({ createdAt: "desc" });
   const basicEventInfo = allEvents.map((singleEvent) => {
     return ({
@@ -285,24 +335,25 @@ app.get("/event/public", async (req, res) => {
     })
   })
   try {
-    res.status(200).json({
-      success: true,
-      response: basicEventInfo
-    })
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      res.status(200).json({
+        success: true,
+        response: allEvents
+      })
+    } else {
+      res.status(200).json({
+        success: true,
+        response: basicEventInfo
+      })
+    }
   } catch (err) {
     res.status(400).json({
       success: false,
       response: err
-    })
+    });
   }
 })
-
-// ENDPOINTS
-app.get("/secret", authenticateUser)
-app.get("/secret", (_, res) => {
-  res.send("YEEEEEY")
-})
-
 
 
 // Start the server
