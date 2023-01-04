c560841052daf5c38b534a6742b78c7ed4968e7c
Elin <elin.segelov@gmail.com>
2023-01-04 23:42:37 +0100

    [es] Add eventTime to create event endpoint, add game image to events. Add styling for eventCard

--- frontend/src/components/userDashboard/events/EventCard.js
+++ frontend/src/components/userDashboard/events/EventCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 const EventCard = ({
+  id,
   game,
   host,
   venue,
@@ -10,17 +11,21 @@ const EventCard = ({
   isFull,
   description,
   eventName,
-  eventTime
+  eventTime,
+  image
 }) => {
   // Om inget eventnamn, visa endast game
   return (
-    <StyledEventCard>
-      <h3>{eventName} - {game}</h3>
-      <p>{venue}</p>
-      <p>{host}</p>
-      <p>{isFull ? 'Event is full' : `${openSpots} / ${totalSpots}`}</p>
-      <p>{description}This is a description</p>
-      <p>{eventTime}</p>
+    <StyledEventCard key={id}>
+      <GameImage src={image} alt={game} />
+      <EventInfo>
+        <h3>{eventName} - {game}</h3>
+        <p><span>Host:</span> {host}</p>
+        <p><span>Where?</span> {venue}</p>
+        <p><span>When?</span> {eventTime}</p>
+        <p><span>Open spots:</span>{isFull ? 'Event is full' : ` ${openSpots} / ${totalSpots}`}</p>
+      </EventInfo>
+      <DescriptionParagraph>{description}</DescriptionParagraph>
     </StyledEventCard>
   )
 }
@@ -29,4 +34,24 @@ export default EventCard
 
 const StyledEventCard = styled.div`
   width: 100%;
+  padding: 0.5rem;
+  display: grid;
+  grid-template-columns: 40% 60%;
+  align-items: center;
+  row-gap: 0.25rem;
+  line-height: 1.2;
+  
+`
+
+const GameImage = styled.img`
+  width: 8rem;
+  
+`
+const EventInfo = styled.div`
+  span, h3 {
+    color: #DE605B;
+  }
+`
+const DescriptionParagraph = styled.p`
+  grid-column: 1 / 3;
 `
