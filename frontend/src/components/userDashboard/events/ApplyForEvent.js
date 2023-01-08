6ad674017aab08a7652afdc668924ee2a1192c76
Elin <elin.segelov@gmail.com>
2023-01-08 09:16:19 +0100

    Add sweetalerts to frontend

--- frontend/src/components/userDashboard/events/ApplyForEvent.js
+++ frontend/src/components/userDashboard/events/ApplyForEvent.js
@@ -4,24 +4,46 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Button1 } from 'styles/Button.styles';
 import { API_URL } from 'utils/utils';
+import Swal from 'sweetalert2';
 
 const ApplyToEvent = ({ eventId }) => {
   const user = useSelector((store) => store.user.userInfo);
-  const sendApplication = () => {
-    console.log('clicked to apply')
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: user.accessToken
-      },
-      body: JSON.stringify({
-        userEmail: user.email,
-        username: user.username,
-        eventId
+
+  const sendApplication = async () => {
+    try {
+      console.log('clicked to apply')
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: user.accessToken
+        },
+        body: JSON.stringify({
+          userEmail: user.email,
+          username: user.username,
+          eventId
+        })
+      }
+      const response = await fetch(API_URL('applyForSpot'), options);
+      const data = await response.json();
+      console.log(data)
+      Swal.fire({
+        title: `${data.response}`,
+        timer: 1400,
+        color: '#DE605B',
+        timerProgressBar: true,
+        showConfirmButton: false
+      })
+    } catch (error) {
+      console.error(error.message)
+      Swal.fire({
+        title: `${error.message}`,
+        timer: 1400,
+        color: '#DE605B',
+        timerProgressBar: true,
+        showConfirmButton: false
       })
     }
-    fetch(API_URL('applyForSpot'), options)
   }
   return (
     <Button1
