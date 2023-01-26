95cb84db8ac7a5e4006d687cc172a2b2713de19a
David B Font <108959957+Fonnt@users.noreply.github.com>
2023-01-26 20:06:46 +0100

    Update urls.js

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -2,8 +2,8 @@
 const BASE_URL = 'https://final-project-dthe36jxxa-lz.a.run.app';
 const URL = 'http://localhost:8080';
 
-export const API_URL = (slug) => `${URL}/${slug}`;
-// export const API_URL = (slug) => `${BASE_URL}/${slug}`;
+// export const API_URL = (slug) => `${URL}/${slug}`;
+export const API_URL = (slug) => `${BASE_URL}/${slug}`;
 
 export const BGG_API_SEARCH_BY_NAME = 'https://boardgamegeek.com/xmlapi/search?search=';
 
