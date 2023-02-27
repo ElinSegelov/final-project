05e0abc157c409f3957e2b9cb2f7c534f09b3b5b
Fonnt <dballesterfont@gmail.com>
2023-02-27 09:19:44 +0100

    [db] Added functionallity for send and accept friend request, checking if they are already friends or the invite has already been sent

--- backend/Endpoints/User/friends/SendFriendRequest.js
+++ backend/Endpoints/User/friends/SendFriendRequest.js
@@ -4,55 +4,76 @@ export const SendFriendRequest = async (req, res) => {
   const { _id } = req.body;
 
   if (_id) {
-    try {
-      const userSendingRequest = await User.findOne({ accessToken: req.header("Authorization") });
-      if (userSendingRequest) {
-        const userGettingRequest = await User.findOne({ _id });
-        if (userGettingRequest) {
+    const userGettingRequest = await User.findOne({ _id });
+    const userSendingRequest = await User.findOne({ accessToken: req.header("Authorization") });
+    if (userSendingRequest) {
+      try {
+        if (userSendingRequest.friendsList.length > 0) {
+          // This checks if the userGetting Requested already is in the friendList of the user Sending the request
+          const findIfAlreadyFriends = userSendingRequest.friendsList.find((friend) => friend.username === userGettingRequest.username);
+
+          if (findIfAlreadyFriends.username === userGettingRequest.username) {
+            res.status(400).json({
+              success: false,
+              response: "Already friends"
+            });
+          }
+        } else if (userSendingRequest.pendingFriends.length > 0) {
+          // This checks if the userSending the request has already sent a request
+          const findIfPending = userSendingRequest.pendingFriends.find((friend) => friend.username === userGettingRequest.username);
+          if (findIfPending.username === userGettingRequest.username) {
+            res.status(400).json({
+              success: false,
+              response: "You have already sent a friend request"
+            });
+          }
+        } else {
           const addingUserSendingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userGettingRequest._id },
-            { $push: { pendingFriends: userSendingRequest } }, { new: true }
-          )
+            { $push: { pendingFriends: { _id: userSendingRequest._id, username: userSendingRequest.username } } }, { new: true }
+          );
           if (addingUserSendingRequestToPendingFriends) {
             const addingUserGettingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userSendingRequest._id },
-              { $push: { pendingFriends: userGettingRequest } }, { new: true }
-            )
+              { $push: { pendingFriends: { _id: userGettingRequest._id, username: userGettingRequest.username } } }, { new: true }
+            );
             if (addingUserSendingRequestToPendingFriends && addingUserGettingRequestToPendingFriends) {
-              res.status(200).json({
+              res.status(201).json({
                 success: true,
                 response: {
-                  userSendingRequest: addingUserSendingRequestToPendingFriends,
-                  userGettingRequest: addingUserGettingRequestToPendingFriends
+                  message: `We have sent a friend request to ${userGettingRequest.username}`,
+                  userSendingRequest: {
+                    _id: userSendingRequest._id,
+                    username: userSendingRequest.username
+                  },
+                  userGettingRequest: {
+                    _id: userGettingRequest._id,
+                    username: userGettingRequest.username
+                  },
                 }
-              })
+              });
             }
           } else {
             res.status(400).json({
               success: false,
-              response: "We couldn't send the friend invitation to this user"
-            })
+              response: "We couldn't send the friend request to this user"
+            });
           }
-        } else {
-          res.status(400).json({
-            success: false,
-            response: "We couldn't send the friend invitation to this user"
-          })
         }
-      } else {
+      } catch (err) {
         res.status(400).json({
           success: false,
-          response: "Please log in"
-        })
+          response: err.stack
+        });
       }
-    } catch (err) {
-      res.status(400).json({
+    } else {
+      res.status(401).json({
         success: false,
-        response: err.stack
-      })
+        response: "Unauthorized request. Please log in"
+      });
     }
   } else {
     res.status(400).json({
       success: false,
       response: "We couldn't find this user to be added as a friend"
-    })
+    });
   }
 }
