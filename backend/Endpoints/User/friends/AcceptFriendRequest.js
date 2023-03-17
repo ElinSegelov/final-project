e95554339d61e662be3b99d55504b6817920e1d1
Fonnt <dballesterfont@gmail.com>
2023-03-17 10:49:07 +0100

    Fixed issue when sending and accepting request with different users

--- backend/Endpoints/User/friends/AcceptFriendRequest.js
+++ backend/Endpoints/User/friends/AcceptFriendRequest.js
@@ -2,85 +2,81 @@ import { User } from "../../../Models";
 
 export const acceptFriendRequest = async (req, res) => {
   const { _id } = req.body;
-
   if (_id) {
     const userGettingAccepted = await User.findOne({ _id });
     const userAcceptingRequest = await User.findOne({ accessToken: req.header("Authorization") });
     if (userAcceptingRequest) {
       try {
-        // This is for checking if a friendRequest is active in pendingFriends. If true, accept is available and it push friendship and 
-        // pulls from pending to both users.
-        if (userAcceptingRequest.pendingFriends.length >= 0) {
-          const findPendingInUserAcceptingRequest = userAcceptingRequest.pendingFriends.find((friend) => friend.username === userGettingAccepted.username);
-          if (findPendingInUserAcceptingRequest) {
-            // This checks if they are already friends.
-            if (userGettingAccepted.friendsList.length > 0) {
-              const findIfAlreadyFriends = userAcceptingRequest.friendsList.find((friend) => friend.username === userGettingAccepted.username);
-              if (findIfAlreadyFriends.username === userGettingAccepted.username) {
-                res.status(400).json({
-                  success: false,
-                  response: "Already friends"
-                });
-              }
-            } else {
-              const addingAcceptedUserToFriendsList = await User.findByIdAndUpdate({ _id: userAcceptingRequest._id },
-                { $push: { friendsList: { _id: userGettingAccepted._id, username: userGettingAccepted.username, friendsAt: new Date() } } }
+        const findPendingInUserAcceptingRequest = userAcceptingRequest.pendingFriends.find((friend) => friend.username === userGettingAccepted.username);
+        console.log(findPendingInUserAcceptingRequest)
+        if (findPendingInUserAcceptingRequest !== undefined) {
+          const findIfAlreadyFriends = userAcceptingRequest.friendsList.find((friend) => friend.username === userGettingAccepted.username);
+          if (findIfAlreadyFriends === undefined) {
+            const addingAcceptedUserToFriendsList = await User.findByIdAndUpdate({ _id: userAcceptingRequest._id },
+              { $push: { friendsList: { _id: userGettingAccepted._id, username: userGettingAccepted.username, friendsAt: new Date() } } }
+            );
+            if (addingAcceptedUserToFriendsList) {
+              const addingUserAcceptingRequestToFriendsList = await User.findByIdAndUpdate({ _id: userGettingAccepted._id },
+                { $push: { friendsList: { _id: userAcceptingRequest._id, username: userAcceptingRequest.username, friendsAt: new Date() } } }
               );
-
-              if (addingAcceptedUserToFriendsList) {
-                const addingUserAcceptingRequestToFriendsList = await User.findByIdAndUpdate({ _id: userGettingAccepted._id },
-                  { $push: { friendsList: { _id: userAcceptingRequest._id, username: userAcceptingRequest.username, friendsAt: new Date() } } }
+              if (addingUserAcceptingRequestToFriendsList && addingAcceptedUserToFriendsList) {
+                const updatedAcceptedUserFromPendingFriends = await User.findByIdAndUpdate({ _id: userAcceptingRequest._id },
+                  { $pull: { pendingFriends: { _id: userGettingAccepted._id } } }, { new: true }
+                );
+                const updatedRequestedUserFromPendingFriends = await User.findByIdAndUpdate({ _id: userGettingAccepted._id },
+                  { $pull: { pendingFriends: { _id: userAcceptingRequest._id } } }, { new: true }
                 );
 
-                if (addingUserAcceptingRequestToFriendsList && addingAcceptedUserToFriendsList) {
-                  const updatedAcceptedUserFromPendingFriends = await User.findByIdAndUpdate({ _id: userAcceptingRequest._id },
-                    { $pull: { pendingFriends: { _id: userGettingAccepted._id } } }, { new: true }
-                  );
-                  const updatedRequestedUserFromPendingFriends = await User.findByIdAndUpdate({ _id: userGettingAccepted._id },
-                    { $pull: { pendingFriends: { _id: userAcceptingRequest._id } } }, { new: true }
-                  );
-
-                  if (updatedAcceptedUserFromPendingFriends && updatedRequestedUserFromPendingFriends) {
-                    res.status(201).json({
-                      success: true,
-                      response: {
-                        message: "You are now friends",
-                        userAcceptingRequest: updatedAcceptedUserFromPendingFriends,
-                        userGettingAccepted: updatedRequestedUserFromPendingFriends
-                      }
-                    });
-                  }
+                if (updatedAcceptedUserFromPendingFriends && updatedRequestedUserFromPendingFriends) {
+                  res.status(201).json({
+                    success: true,
+                    response: {
+                      message: "You are now friends",
+                      userAcceptingRequest: updatedAcceptedUserFromPendingFriends,
+                      userGettingAccepted: updatedRequestedUserFromPendingFriends
+                    }
+                  });
+                } else {
+                  res.status(400).json({
+                    success: false,
+                    response: "Something went wrong"
+                  });
                 }
               }
+            } else {
+              res.status(400).json({
+                success: false,
+                response: "We couldn't add this user as a friend"
+              });
             }
           } else {
             res.status(400).json({
               success: false,
-              response: "No friend request was found."
-            })
+              response: "Already friends"
+            });
           }
         } else {
           res.status(400).json({
             success: false,
-            response: "Something went wrong"
-          })
+            response: "No friend request was found."
+          });
         }
       } catch (err) {
         res.status(400).json({
           success: false,
           response: err.stack
-        });
+        })
       }
     } else {
       res.status(401).json({
         success: false,
         response: "Unauthorized request. Please log in"
-      });
+      })
     }
   } else {
     res.status(400).json({
       success: false,
-      response: "Please provide the user ID for sending the request"
+      response: "Please provide the user ID that you want to delete"
     })
   }
 }
--- backend/Endpoints/User/friends/SendFriendRequest.js
+++ backend/Endpoints/User/friends/SendFriendRequest.js
@@ -8,72 +8,68 @@ export const sendFriendRequest = async (req, res) => {
     const userSendingRequest = await User.findOne({ accessToken: req.header("Authorization") });
     if (userSendingRequest) {
       try {
-        if (userSendingRequest.friendsList.length > 0) {
-          // This checks if the userGetting Requested is already in the friendList of the user Sending the request
+        const findIfPending = userSendingRequest.pendingFriends.find((friend) => friend.username === userGettingRequest.username);
+        console.log(findIfPending)
+        if (findIfPending === undefined) {
           const findIfAlreadyFriends = userSendingRequest.friendsList.find((friend) => friend.username === userGettingRequest.username);
-
-          if (findIfAlreadyFriends.username === userGettingRequest.username) {
-            res.status(400).json({
-              success: false,
-              response: "Already friends"
-            });
-          }
-        } else if (userSendingRequest.pendingFriends.length > 0) {
-          // This checks if the userSending the request has already sent a request
-          const findIfPending = userSendingRequest.pendingFriends.find((friend) => friend.username === userGettingRequest.username);
-          if (findIfPending.username === userGettingRequest.username) {
-            res.status(400).json({
-              success: false,
-              response: "You have already sent a friend request"
-            });
-          }
-        } else {
-          const addingUserSendingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userGettingRequest._id },
-            { $push: { pendingFriends: { _id: userSendingRequest._id, username: userSendingRequest.username } } }, { new: true }
-          );
-          if (addingUserSendingRequestToPendingFriends) {
-            const addingUserGettingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userSendingRequest._id },
-              { $push: { pendingFriends: { _id: userGettingRequest._id, username: userGettingRequest.username } } }, { new: true }
+          if (findIfAlreadyFriends === undefined) {
+            const addingUserSendingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userGettingRequest._id },
+              { $push: { pendingFriends: { _id: userSendingRequest._id, username: userSendingRequest.username } } }, { new: true }
             );
-            if (addingUserSendingRequestToPendingFriends && addingUserGettingRequestToPendingFriends) {
-              res.status(201).json({
-                success: true,
-                response: {
-                  message: `We have sent a friend request to ${userGettingRequest.username}`,
-                  userSendingRequest: {
-                    _id: userSendingRequest._id,
-                    username: userSendingRequest.username
-                  },
-                  userGettingRequest: {
-                    _id: userGettingRequest._id,
-                    username: userGettingRequest.username
-                  },
-                }
+            if (addingUserSendingRequestToPendingFriends) {
+              const addingUserGettingRequestToPendingFriends = await User.findByIdAndUpdate({ _id: userSendingRequest._id },
+                { $push: { pendingFriends: { _id: userGettingRequest._id, username: userGettingRequest.username } } }, { new: true }
+              );
+              if (addingUserSendingRequestToPendingFriends && addingUserGettingRequestToPendingFriends) {
+                res.status(201).json({
+                  success: true,
+                  response: {
+                    message: `We have sent a friend request to ${userGettingRequest.username}`,
+                    userSendingRequest: {
+                      _id: userSendingRequest._id,
+                      username: userSendingRequest.username
+                    },
+                    userGettingRequest: {
+                      _id: userGettingRequest._id,
+                      username: userGettingRequest.username
+                    },
+                  }
+                });
+              }
+            } else {
+              res.status(400).json({
+                success: false,
+                response: "We couldn't send the friend request to this user"
               });
             }
           } else {
             res.status(400).json({
               success: false,
-              response: "We couldn't send the friend request to this user"
+              response: "Already friends"
             });
           }
+        } else {
+          res.status(400).json({
+            success: false,
+            response: "You have already sent a friend request"
+          });
         }
       } catch (err) {
         res.status(400).json({
           success: false,
           response: err.stack
-        });
+        })
       }
     } else {
       res.status(401).json({
         success: false,
         response: "Unauthorized request. Please log in"
-      });
+      })
     }
   } else {
     res.status(400).json({
       success: false,
-      response: "We couldn't find this user to be added as a friend"
-    });
+      response: "Please provide the user ID that you want to delete"
+    })
   }
 }
